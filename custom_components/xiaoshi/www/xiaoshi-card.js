9485a0442f3f0a2a18091582d43de28035f7d54b
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-06-02 00:42:28 +0800

    Update xiaoshi-card.js

--- custom_components/xiaoshi/www/xiaoshi-card.js
+++ custom_components/xiaoshi/www/xiaoshi-card.js
@@ -1,3 +1,5 @@
+console.info("%c 消逝卡-汇总卡 \n%c        v 2.0 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+
 import('./xiaoshi/phone-card.js');
 import('./xiaoshi/avatar-card.js');
 import('./xiaoshi/dynamic-card.js');
@@ -9,12 +11,20 @@ import('./xiaoshi-card/chart-button.js');
 import('./xiaoshi-card/ha-info-button.js'); 
 import('./xiaoshi-card/balance-button.js');
 import('./xiaoshi-card/consumables-button.js');
+import('./xiaoshi-card/custom-petrochina-button.js');
+import('./xiaoshi-card/custom-qinhua-gas-button.js');
+import('./xiaoshi-card/custom-state-grid-button.js');
+import('./xiaoshi-card/custom-xian-water-button.js');
 
 import('./xiaoshi-card/todo-card.js');
 import('./xiaoshi-card/chart-card.js');
 import('./xiaoshi-card/ha-info-card.js'); 
 import('./xiaoshi-card/balance-card.js');
 import('./xiaoshi-card/consumables-card.js');
+import('./xiaoshi-card/custom-petrochina-card.js');
+import('./xiaoshi-card/custom-qinhua-gas-card.js');
+import('./xiaoshi-card/custom-state-grid-card.js');
+import('./xiaoshi-card/custom-xian-water-card.js');
 
 import('./xiaoshi-phone/climate-card.js');
 import('./xiaoshi-phone/humidifier-card.js');
@@ -31,6 +41,3 @@ import('./xiaoshi-small/purifier-card.js');
 
 import('./xiaoshi-pad/climate-card.js');
 import('./xiaoshi-pad/grid-card.js');
-
-
-console.info("%c 消逝卡-汇总卡 \n%c        v 1.9 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
