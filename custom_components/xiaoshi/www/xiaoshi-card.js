f9c061748b2efb4475f381202539dc56e22087f9
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-06-14 01:54:28 +0800

    Add files via upload

--- custom_components/xiaoshi/www/xiaoshi-card.js
+++ custom_components/xiaoshi/www/xiaoshi-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝卡-汇总卡 \n%c  2026.6.2.13 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-汇总卡 \n%c  2026.6.2.14 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 import('./function/function.js');
 import('./xiaoshi/phone-card.js');
@@ -25,8 +25,9 @@ import('./xiaoshi-phone/computer-card.js');
 import('./xiaoshi-phone/light-card.js');
 import('./xiaoshi-phone/switch-card.js');
 import('./xiaoshi-phone/music-player-card.js');
-import('./xiaoshi-phone/other-card.js');
 import('./xiaoshi-phone/fan-card.js');
+import('./xiaoshi-phone/cover-card.js');
+import('./xiaoshi-phone/other-card.js');
 
 import('./xiaoshi-small/climate-card.js');
 import('./xiaoshi-small/humidifier-card.js');
