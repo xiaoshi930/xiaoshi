4c81ca26e16a9ba3b8949581889426312355d160
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-06-18 21:24:07 +0800

    Add files via upload

--- custom_components/xiaoshi/www/xiaoshi-card.js
+++ custom_components/xiaoshi/www/xiaoshi-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝卡-汇总卡 \n%c  2026.6.3.14 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-汇总卡 \n%c  2026.6.3.15 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 import('./function/function.js');
 import('./xiaoshi/phone-card.js');
@@ -19,6 +19,7 @@ import('./xiaoshi-card/button.js');
 import('./xiaoshi-card/device.js');
 import('./xiaoshi-card/light.js');
 import('./xiaoshi-card/iot-computer-card.js');
+import('./xiaoshi-card/pve-card.js');
 
 import('./xiaoshi-phone/climate-card.js');
 import('./xiaoshi-phone/humidifier-card.js');
