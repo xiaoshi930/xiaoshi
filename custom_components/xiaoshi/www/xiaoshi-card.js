5d0565002aa1e66ac65b54552dfce69a3c7423c9
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-07-01 00:47:51 +0800

    Update xiaoshi-card.js

--- custom_components/xiaoshi/www/xiaoshi-card.js
+++ custom_components/xiaoshi/www/xiaoshi-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝卡-汇总卡 \n%c  2026.6.4.27 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
+console.info("%c 消逝卡-汇总卡 \n%c  2026.6.4.28 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: black");
 
 import('./function/function.js');
 import('./xiaoshi/phone-card.js');
